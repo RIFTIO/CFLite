9122e282c1dae63c396db1ee40ea1f8d462b3f52
Balaji Rajappa <balaji.rajappa@dzsi.com>
2024-01-17 12:17:07 +0000

    Ubuntu 22.04 port gi fix

--- CoreFoundation/CFDate.h
+++ CoreFoundation/CFDate.h
@@ -2,14 +2,14 @@
  * Copyright (c) 2012 Apple Inc. All rights reserved.
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -17,7 +17,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 
@@ -38,13 +38,8 @@ typedef CFTimeInterval CFAbsoluteTime;
 /* absolute time is the time interval since the reference date */
 /* the reference date (epoch) is 00:00:00 1 January 2001. */
 
-/**
- * CFAbsoluteTimeGetCurrent:
- *
- * Returns: (transfer none)
- */
 CF_EXPORT
-CFAbsoluteTime CFAbsoluteTimeGetCurrent(void);
+CFTimeInterval CFAbsoluteTimeGetCurrent(void);
 
 CF_EXPORT
 const CFTimeInterval kCFAbsoluteTimeIntervalSince1970;
